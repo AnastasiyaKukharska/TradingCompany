6ab114862b56cca26fdf420715700ad381ee3d00
AnastasiyaKukharska <70960052+AnastasiyaKukharska@users.noreply.github.com>
2020-10-03 03:23:52 +0300

    DAL

---
+++ Interfaces/IBasketDal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+
+namespace DAL.Interfaces
+{
+    public interface IBasketDal
+    {
+        void GetBasketById(int id);
+        List<BasketDTO> GetAllBaskets();
+        void  UpdateBasket(int id,int Sid);
+       BasketDTO CreateBasket (BasketDTO basket);
+        void DeleteBasket(int id);
+    }
+}
---
+++ Interfaces/ICategoriesDal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+
+namespace DAL
+{
+    public interface ICategoriesDal
+    {
+        List<CategoriesDTO> GetAllCategories();
+        //CategoriesDTO UpdateCategory(CategoriesDTO category);
+        //CategoriesDTO CreateCategory(CategoriesDTO category);
+        //void DeleteCategories(int id);
+
+    }
+}
