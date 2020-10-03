52234133fdcdbbbdcb5bfa48d17fdacf07b9a481
AnastasiyaKukharska <70960052+AnastasiyaKukharska@users.noreply.github.com>
2020-10-03 03:19:54 +0300

    DTO

---
+++ StatusDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class StatusDTO
+    {
+        public int StatusID { get; set; }
+        public string Done { get; set; }
+        public string Active { get; set; }
+    }
+}
