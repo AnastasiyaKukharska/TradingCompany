cee87555401731ee0cc95d89a7c2b8dabd4aad92
AnastasiyaKukharska <70960052+AnastasiyaKukharska@users.noreply.github.com>
2020-10-28 23:46:04 +0200

    Add files via upload

---
+++ Dalef/Concrete/CategoryDalef.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using DAL.Interfaces;
+using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace DALef.Concrete
+{
+    public class CategoriesDalEf : ICategoriesDal
+    {
+        private readonly IMapper _mapper;
+
+        public CategoriesDalEf(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public List<CategoriesDTO> GetAllCategories()
+        {
+            using (var entities = new IMDBEntities())
+            {
+                var categories = entities.Category.ToList();
+                return _mapper.Map<List<CategoriesDTO>>(categories);
+            }
+        }
+    }
+}
